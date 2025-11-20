762cdeb21a9f48883cd169223eadf000b52dfb66
qwertovv <muhiniljusha@yandex.ru>
2025-11-20 10:25:08 +0300

    Добавление конвертеров
    Добавлен BoolToColorConverter

---
+++ SmartKettle/Converters/BoolToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SmartKettle.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Brushes.LightGreen : Brushes.LightCoral;
+            }
+            return Brushes.LightGray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
