69e8847f34dabf282aaf79d78814b5aaa067c50d
qwertovv <muhiniljusha@yandex.ru>
2025-11-20 10:27:14 +0300

    Добавление RelayCommand

---
+++ SmartKettle/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SmartKettle
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) => canExecute?.Invoke() ?? true;
+
+        public void Execute(object parameter) => execute();
+    }
+}
